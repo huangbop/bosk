301a7985b2f804dca497d6ee8dcd9f69f0a33bfa
Huang Bo <huangbop@gmail.com>
2015-01-19 16:56:39 +0800

    Get the command and will dispatch

--- management/__init__.py
+++ management/__init__.py
@@ -1,23 +1,25 @@
 import os
 import sys
 from argparse import ArgumentParser
+import collections
 
 
-def find_extern_commands(management_dir):
+def get_extern_commands_dict(management_dir):
     """
     List the names in management/commands, without .py
+
+    return  {cmd_name: app, ...}
     """
     commands_dir = os.path.join(management_dir, 'commands')
     try:
-        pass
+        commands =  [f[:-3] for f in os.listdir(commands_dir)
+                     if not f.startswith('-') and f.endswith('.py')]
+        # This commands are all in management app
+        return {cmd: 'management' for cmd in commands}
+        
     except OSError:
-        return []
-    
-    
-
-    
-
-
+        return {}
+        
 
 
 class BoskArgumentParser(ArgumentParser):
@@ -27,9 +29,23 @@ class BoskArgumentParser(ArgumentParser):
         
     def print_help(self):
         ArgumentParser.print_help(self)
-        print('\nI stand here')
         
+        # Print extern commands help
+        usage = ["",
+                 "Valid commands list blew:",]
+        commands = get_extern_commands_dict(__path__[0])
+        commands_ddict = collections.defaultdict(lambda: [])
+        for cmd in sorted(commands.keys()):
+            commands_ddict[commands[cmd]].append(cmd)
 
+        for app in sorted(commands_ddict.keys()):
+            usage.append("")
+            usage.append("[%s]" % app)
+            for cmd in sorted(commands_ddict[app]):
+                usage.append("    %s" % cmd)
+
+        sys.stdout.write('\n'.join(usage))
+        
 
 class ManagementUtil():
     """
@@ -37,13 +53,27 @@ class ManagementUtil():
     """
     def __init__(self):
         self.argv = sys.argv
+        self.prog_name = os.path.basename(self.argv[0])
         
         
     def run(self):
         parser = BoskArgumentParser()
         parser.add_argument('command', help='Bosk top level commnad')
         parser.add_argument('-v', '--verbose', help='Show verbose information')
-        command_dict = vars(parser.parse_args(self.argv[1:]))
+        parse_dict = vars(parser.parse_args(self.argv[1:]))
+
+        self.fetch_command(parse_dict['command']).run_from_argv(self.argv)
+
+
+    def fetch_command(self, command):
+        extern_commands = get_extern_commands_dict(__path__[0])
+        try:
+            app_name = extern_commands[command]
+        except KeyError:
+            sys.stderr.write("Unknown command: %s\n" % command)
+            sys.exit(1)
+
+        print('I will work here.\n')
 
             
 def run_commands():
